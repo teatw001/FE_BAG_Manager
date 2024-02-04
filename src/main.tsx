e83228aade62f3fb2edb442e2cd77dba2cfe418b
teatw001 <duydhph26793@fpt.edu.vn>
2024-02-04 19:17:35 +0700

    test

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+// import { PersistGate } from "redux-persist/integration/react";
+import App from "./App.tsx";
+import "./index.css";
+import { store } from "./store/store.ts";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-)
+  </Provider>
+);
